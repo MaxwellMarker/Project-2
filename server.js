5701481c0bb35bd3d1a2d629745711f2122ef0f9
Max Marker <maxmarker98@gmail.com>
2020-08-20 11:39:49 -0400

    basic setup complete with env and git ignore

---
+++ server.js
@@ -0,0 +1,37 @@
+//Dependencies
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
+const mongoURI = process.env.MONGO_URI;
+
+//Mongo connection
+mongoose.connect(mongoURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }
+);
+mongoose.connection.once('open', () => {
+    console.log("connected to mongo");
+})
+
+//Controllers
+app.get('/', (req, res) => {
+    res.send('hello father');
+})
+
+//Listen
+app.listen(PORT, () => {
+    console.log('listening on ' + PORT);
+})
